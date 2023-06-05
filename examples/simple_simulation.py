b8b78f5794cbc8dfec3b15c9ffd6c977b314c2c2
nickwoods2 <nick.woods@deliveroo.com>
2023-06-05 02:42:43 +0100

    Brain functionality, death chance, reproduction, etc.

--- examples/simple_simulation.py
+++ examples/simple_simulation.py
@@ -1,5 +1,7 @@
 # examples/simple_simulation.py
 import sys
+import numpy as np
+import os
 
 sys.path.append("..")  # add the parent directory to the Python path
 
@@ -7,41 +9,31 @@ sys.path.append("..")  # add the parent directory to the Python path
 from src.organisms import Organism
 from src.environment import Environment
 from src.simulation import Simulation
-from src.genetics import Neuron, Gene, Genes
 
 
 def run_simple_simulation():
     # Create a 10x10 environment
-    environment = Environment(10, 10)
+    environment = Environment(20, 20)
 
-    # Define some basic genes for the organisms
-    input_neurons = [Neuron() for _ in range(5)]  # example, modify as necessary
-    intermediary_neurons = [Neuron() for _ in range(5)]  # example, modify as necessary
-    output_neurons = [Neuron() for _ in range(5)]  # example, modify as necessary
+    # Set up a genome that biases the organism to move up
+    genome_up = [
+        0
+    ] * 244  # 234 is the total size of the genome, 0 is arbitrary initialization
+    genome_up[-4] = 1.0  # Bias towards "move up" action in the output layer
+    genome1 = genome_up
 
-    # Let's connect every input neuron to every intermediary neuron, and every
-    # intermediary neuron to every output neuron, with weight 0.5 for simplicity
-    genes_list = []
-    for input_neuron in input_neurons:
-        for inter_neuron in intermediary_neurons:
-            genes_list.append(Gene(input_neuron, inter_neuron, 0.5))
+    genome2 = list(np.random.uniform(low=-1.0, high=1.0, size=(244,)))
+    genome3 = list(np.random.uniform(low=-1.0, high=1.0, size=(244,)))
 
-    for inter_neuron in intermediary_neurons:
-        for output_neuron in output_neurons:
-            genes_list.append(Gene(inter_neuron, output_neuron, 0.5))
-
-    genes = Genes(input_neurons, intermediary_neurons, output_neurons, genes_list)
-
-    # Create some organisms with these genes
-    organism1 = Organism(1, 1, environment, genes)
-    organism2 = Organism(3, 7, environment, genes)
-    organism3 = Organism(9, 5, environment, genes)
+    organism1 = Organism(1, 1, environment, genome1)
+    organism2 = Organism(3, 7, environment, genome2)
+    organism3 = Organism(9, 5, environment, genome3)
 
     # Create a simulation
     simulation = Simulation(environment, [organism1, organism2, organism3])
 
     # Plot the initial state of the environment
-    environment.plot(0)
+    # environment.plot(0)
 
     # Run the simulation for 20 time steps
     simulation.run(20)
